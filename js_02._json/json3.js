171b656827de42a137a69b415e50b88d58cf394a
A-DDONG <ssc5924@gmail.com>
2023-06-13 16:14:21 +0900

    fetch

---
+++ js_02._json/json3.js
@@ -0,0 +1,4 @@
+// JS 에서 AJAX 방식으로 BackEnd Server 로부터 데이터를 받아오는 함수
+fetch("https://jsonplaceholder.typicode.com/todos")
+  .then((response) => response.json())
+  .then((json) => console.log(json));
